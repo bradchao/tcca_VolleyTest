b848691b7eb7f4fa44af38fdb3fde012874e4afa
Brad <brad@brad.tw>
2019-08-01 20:41:46 +0800

    02

--- app/src/main/java/tw/org/tcca/app/volleytest/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/volleytest/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
+    private MainApp mainApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainApp = (MainApp)getApplication();
+
     }
 
     public void getTest1(View view) {
---
+++ app/src/main/java/tw/org/tcca/app/volleytest/MainApp.java
@@ -0,0 +1,16 @@
+package tw.org.tcca.app.volleytest;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MainApp extends Application {
+    public RequestQueue queue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        queue = Volley.newRequestQueue(this);
+    }
+}
